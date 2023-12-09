fb7d3023bb0e405c6097fee07286c12f45161a11
Adam <apa69@sfu.ca>
2023-12-09 12:28:12 -0800

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-10">
+          Stemming from a love of sports from a young age, I thrive in team environments 
+          and have strong communication and leadership skills. I am highly self motivated, 
+          passionate about software development as much as anything. When I'm not working 
+          I love to play or watch sports with friends, go for a run, or play video games!
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Having graduated earlier this year, I'm seeking a new role as a web developer.
+          I have over a year of professional experience developing front and backend
+          systems and am open to all opportunities!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import d2 from "../assets/portfolio/destiny2.png";
+import running from "../assets/portfolio/running.png";
+import nba from "../assets/portfolio/nba.png";
+import puzzle from "../assets/portfolio/puzzle.png";
+
+const Portfolio = () => {
+  const projects = [
+    {
+      id: 1,
+      image: d2,
+      link: "http://pimfr.s3-website.us-east-2.amazonaws.com/",
+      code: "https://github.com/adpetes/petes-item-manager",
+      desc: "A player-aid for the game Destiny 2. Rather than create new projects, I seem to just keep adding to this one",
+    },
+    {
+      id: 2,
+      image: running,
+      link: "https://adamsrunningstuff.netlify.app/",
+      code: "https://github.com/adpetes/RunningProgressApp",
+      desc: "A hub for info about my biggest hobby - running! Contains my daily workout routine, best race times, and more",
+    },
+    {
+      id: 3,
+      image: nba,
+      code: "https://github.com/adpetes/NBA_predict_games",
+      desc: "An in-progress machine learning project which aims to predict the outcome of NBA games!",
+    },
+    {
+      id: 4,
+      image: puzzle,
+      code: "https://github.com/adpetes/sliding-puzzle",
+      desc: "An android sliding puzzle game with an AI 'solver' tool which shows an optimal solution to any instance of the puzzle",
+    }
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full md:h-screen text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map((project) => (
+            <div key={project.id} className=" shadow-md shadow-gray-600 rounded-lg">
+              <div className="rounded-md flex flex-col items-center"> 
+                <img
+                  src={project.image}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105 max-h-20 mt-4"
+                />
+                <p className="text-center mt-2 mx-2">{project.desc}</p>
+              </div>
+              <div className="flex items-center justify-center">
+                {project.link && <a href={project.link} target="_blank" className="px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </a>}
+                <a href={project.code} target="_blank" className="px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
